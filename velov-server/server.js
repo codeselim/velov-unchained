4d2b1d2c347c46ca81328e1ba7a72be028263e68
tdubourg <plop@plop.fake>
2014-01-20 22:31:23 +0100

    WIP on velov server implementation. Added functions in order to decode the frame + a skeleton of something in order to process incoming data

--- velov-server/server.js
+++ velov-server/server.js
@@ -10,6 +10,7 @@ var DATA_SEPARATOR = "\t"
 var sha1sum = require('crypto').createHash('sha1')
 var VELOV_CONNECTION_PORT = 5000
 var VELOV_MESSAGE_FAILED_RETRY_TIME = 10000 // milliseconds
+var CMD_LEN = 3 // length of the string expressing the  "command" in a frame from the velov
 
 var sha1 = function (string) {
 	sha1sum.update(string)
@@ -17,17 +18,60 @@ var sha1 = function (string) {
 }
 
 var decode = function (frame) {
-	return frame
+	var data = get_data_from_frame(frame)
+	var type = get_type_from_data(data)
+	var cmd = get_cmd_from_data(data)
+	var params = get_params_from_data(data)
+	return {
+		  'type': type
+		, 'cmd': cmd
+		, 'params': params
+	}
+}
+
+/**
+ * @return the command, extracted from the data, such as "CHG" for a state change, "LOC" for a localization command, etc. ...
+*/
+var get_cmd_from_data = function (data) {
+	return data.substr(0, CMD_LEN)
+}
+
+
+/**
+ * @return the type of the frame, extracted from the data, such as "CHG" for a state change, "LOC" for a localization command, etc. ...
+*/
+var get_type_from_data = function (data) {
+	 // current implementation is the same as returning the cmd but we might want 
+	 // to change this in the future so we keep this a separate function
+	return get_cmd_from_data(data)
+}
+
+/**
+@return an array of space-separated parameters of the given data
+*/
+var get_params_from_data = function (data) {
+	return data.substr(CMD_LEN+1, data.length).split(" ")
 }
 
 var checksum = function (data) {
 	return sha1(data)
 }
 
-var check_checksum = function (frame) {
+var get_data_from_frame = function (frame) {
 	var data_end_pos = frame.indexOf(DATA_SEPARATOR)
 	var data = frame.substr(0, data_end_pos)
+	return data
+}
+
+var get_checksum_from_frame = function (frame) {
+	var data_end_pos = frame.indexOf(DATA_SEPARATOR)
 	var data_checksum = frame.substr(data_end_pos + data_end_pos.length, frame.length)
+	return data_checksum
+}
+
+var check_checksum = function (frame) {
+	var data = get_data_from_frame(frame)
+	var data_checksum = get_checksum_from_frame(frame)
 	return (checksum(data) === data_checksum)
 }
 
@@ -67,6 +111,17 @@ var message_velov = function (velov, data, callback, tries_count) {
 	})
 }
 
+var action_localization = function (frame_data) {
+	frame_data.params
+}
+var frame_actions = {
+	"LOC": action_localization
+}
+
+var frame_action = function (frame_data) {
+	frame_actions[frame_data.frame_type](frame_data)
+}
+
 function start (db, port) {
 	var server = net.createServer(function(stream) {
 		stream.setTimeout(0);
@@ -89,6 +144,7 @@ function start (db, port) {
 				var frame = buffer.substr(0, pos)
 				buffer = buffer.substr(pos + FRAME_SEPARATOR.length, buffer.length) //* If the second parameter is >= the maximum possible length substr can return, substr just returns the maximum length possible, so who cares substracting?
 				var frame_data = decode(frame)
+				frame_action(frame_data)
 			};
 			console.log("VSERV: ", "Ending the velovs stream data receiver function") //* Mainly for the purpose of being able to check when the VELOV_FRAME_EVENT handler function is executed with respect to the current function execution
 		});
