327d336f44d0072781bde9363f9aa1f45370cd1b
tdubourg <plop@plop.fake>
2014-01-21 00:01:30 +0100

    WIP on getting the server to act when receiving things from velovs. Worked on having some PGSQL handy functions

--- velov-server/pgsql.js
+++ velov-server/pgsql.js
@@ -3,26 +3,41 @@
 var pg = require('pg');
 var conString = "postgres://velovunchained:velovunchained@localhost/velovunchained";
 
-pg.connect(conString, function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
+var client = new pg.Client(conString)
 
-  client.query('INSERT INTO task_types (name) VALUES (\'Hey\')', [], function(err, result) {
+client.connect()
 
-    if(err) {
-      return console.error('error running query', err);
-    }
-    //output: 1
-  });
+var insert_query = function (table_name, columns, values, callback) {
+	var values_params = []
+	var comma = ''
+	for (var i = 0; i < values.length; i++) {
+		values_params += comma + "$" + i
+		comma = ', '
+	};
+	client.query("INSERT INTO " + table_name + " (" + columns.join(", ") + ") VALUES (" + values_params + ")", values , callback)
+}
 
-  client.query('INSERT INTO velov_tasks (type) VALUES ($1::int)', ['1'], function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
+var update_query = function (table_name, columns, values, callback) {
+	var values_params = []
+	var comma = ''
+	for (var i = 0; i < values.length; i++) {
+		values_params += comma + columns[i] + "=$" + i
+		comma = ', '
+	};
+	client.query("UPDATE " + table_name + " SET " + values_params, values , callback)
+}
 
-    if(err) {
-      return console.error('error running query', err);
-    }
-    //output: 1
-  });
-});
+var delete_query = function (table_name, columns, values, operator, callback) {
+	var values_params = []
+	if (typeof operator == "undefined" || !operator) {
+		var operator = " AND "
+	} else {
+		operator = " " + operator + " "
+	}
+	var comma = ''
+	for (var i = 0; i < values.length; i++) {
+		values_params += comma + columns[i] + "=$" + i
+		comma = operator
+	};
+	client.query("DELETE FROM " + table_name + " WHERE " + values_params, values , callback)
+}
