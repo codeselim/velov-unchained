49affd66f9dd96b511014dcfb214fe30258ca97c
tdubourg <plop@plop.fake>
2014-01-21 23:40:29 +0100

    Updated the SQL schema + added some data

--- sql/data.sql
+++ sql/data.sql
@@ -1,8 +1,17 @@
-INSERT INTO tasks_types (id, name) VALUES (1, "Change state to Unlockable");
-INSERT INTO tasks_types (id, name) VALUES (2, "Change state to Available");
-INSERT INTO tasks_types (id, name) VALUES (3, "Change state to Not Available");
-INSERT INTO tasks_types (id, name) VALUES (4, "Change state to Not Available");
-INSERT INTO tasks_types (id, name) VALUES (5, "Change state to In Maintenance");
-
+INSERT INTO task_types (id, name) VALUES (1, 'Change state to Unlockable');
+INSERT INTO task_types (id, name) VALUES (2, 'Change state to Available');
+INSERT INTO task_types (id, name) VALUES (3, 'Change state to Not Available');
+INSERT INTO task_types (id, name) VALUES (4, 'Change state to Not Available');
+INSERT INTO task_types (id, name) VALUES (5, 'Change state to In Maintenance');
 
 SELECT pg_catalog.setval('task_types_id_seq', 6, true);
+
+-- ---------------------------------------------- --------------------------------------
+
+INSERT INTO task_states (id, codename, name) VALUES (1, 'todo', 'Task to be picked up and processed');
+INSERT INTO task_states (id, codename, name) VALUES (2, 'inprogress', 'The task is currently being processed / executed.');
+INSERT INTO task_states (id, codename, name) VALUES (3, 'success', 'The task has sucessfully completed.');
+INSERT INTO task_states (id, codename, name) VALUES (4, 'failure', 'The task could not be completed successfully.');
+
+
+SELECT pg_catalog.setval('task_types_id_seq', 5, true);
--- sql/schema.sql
+++ sql/schema.sql
@@ -20,33 +20,50 @@ SET search_path = public, pg_catalog;
 SET default_tablespace = '';
 
 SET default_with_oids = false;
+-- --------------------------------------------------------------
+-- --------------------------------------------------------------
 
 CREATE TABLE task_types (
     id integer NOT NULL,
-    name character(10)
+    name varchar(100) NOT NULL
 );
-
-
 ALTER TABLE public.task_types OWNER TO velovunchained;
-
 CREATE SEQUENCE task_types_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
 ALTER TABLE public.task_types_id_seq OWNER TO velovunchained;
-
-
 ALTER SEQUENCE task_types_id_seq OWNED BY task_types.id;
 ALTER TABLE ONLY task_types ALTER COLUMN id SET DEFAULT nextval('task_types_id_seq'::regclass);
+ALTER TABLE ONLY task_types ADD CONSTRAINT task_types_pk PRIMARY KEY (id);
 
 -- --------------------------------- ---------------------------------
+
+CREATE TABLE task_states (
+    id integer NOT NULL,
+    codename varchar(20) NOT NULL,
+    name varchar(100) NOT NULL
+);
+ALTER TABLE public.task_states OWNER TO velovunchained;
+CREATE SEQUENCE task_states_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+ALTER TABLE ONLY task_states ADD CONSTRAINT tasks_pk PRIMARY KEY (id);
+ALTER TABLE public.task_states_id_seq OWNER TO velovunchained;
+ALTER SEQUENCE task_states_id_seq OWNED BY task_states.id;
+ALTER TABLE ONLY task_states ALTER COLUMN id SET DEFAULT nextval('task_states_id_seq'::regclass);
+
+-- --------------------------------- ---------------------------------
+
 CREATE TABLE velov_tasks (
     id integer NOT NULL,
-    type integer NOT NULL
+    type integer NOT NULL,
+    task_state_id integer NOT NULL
 );
 ALTER TABLE public.velov_tasks OWNER TO velovunchained;
 CREATE SEQUENCE velov_tasks_id_seq
@@ -55,11 +72,11 @@ CREATE SEQUENCE velov_tasks_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-ALTER TABLE ONLY task_types ADD CONSTRAINT task_types_pk PRIMARY KEY (id);
 ALTER TABLE ONLY velov_tasks ADD CONSTRAINT tasks_pk PRIMARY KEY (id);
 ALTER TABLE public.velov_tasks_id_seq OWNER TO velovunchained;
 ALTER SEQUENCE velov_tasks_id_seq OWNED BY velov_tasks.id;
 ALTER TABLE ONLY velov_tasks ADD CONSTRAINT task_type_fk FOREIGN KEY (type) REFERENCES task_types(id);
+ALTER TABLE ONLY velov_tasks ADD CONSTRAINT task_state_id_fk FOREIGN KEY (task_state_id) REFERENCES task_states(id);
 ALTER TABLE ONLY velov_tasks ALTER COLUMN id SET DEFAULT nextval('velov_tasks_id_seq'::regclass);
 
 -- --------------------------------- ---------------------------------
@@ -144,67 +161,3 @@ ALTER TABLE ONLY velov_state_history ADD CONSTRAINT velov_state_history_state_id
 ALTER TABLE ONLY velov_state_history ALTER COLUMN id SET DEFAULT nextval('velov_state_history_id_seq'::regclass);
 
 -- --------------------------------- ---------------------------------
-
--- --
--- -- Name: id; Type: DEFAULT; Schema: public; Owner: velovunchained
--- --
-
-
-
--- --
--- -- Name: id; Type: DEFAULT; Schema: public; Owner: velovunchained
--- --
-
-
---
--- Data for Name: task_types; Type: TABLE DATA; Schema: public; Owner: velovunchained
---
-
--- COPY task_types (id, name) FROM stdin;
--- 1    Hey       
--- 2    Hey       
--- \.
-
-
--- --
--- -- Name: task_types_id_seq; Type: SEQUENCE SET; Schema: public; Owner: velovunchained
--- --
-
--- SELECT pg_catalog.setval('task_types_id_seq', 2, true);
-
-
--- --
--- -- Data for Name: velov_tasks; Type: TABLE DATA; Schema: public; Owner: velovunchained
--- --
-
--- COPY velov_tasks (id, type) FROM stdin;
--- 7	1
--- \.
-
-
--- --
--- -- Name: velov_tasks_id_seq; Type: SEQUENCE SET; Schema: public; Owner: velovunchained
--- --
-
--- SELECT pg_catalog.setval('velov_tasks_id_seq', 7, true);
-
-
--- --
--- -- Name: task_types_pk; Type: CONSTRAINT; Schema: public; Owner: velovunchained; Tablespace: 
--- --
-
-
--- --
--- -- Name: public; Type: ACL; Schema: -; Owner: postgres
--- --
-
--- REVOKE ALL ON SCHEMA public FROM PUBLIC;
--- REVOKE ALL ON SCHEMA public FROM postgres;
--- GRANT ALL ON SCHEMA public TO postgres;
--- GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
---
--- PostgreSQL database dump complete
---
-
