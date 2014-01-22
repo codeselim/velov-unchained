3694cfd3a79b5d5bedff10cd392f6bfc8cda61d5
fayperso@gmail.com <fayperso@gmail.com>
2014-01-22 15:25:10 +0100

    Ajout du système embarqué

---
+++ velov-embedded/cmd_interpreter_interface.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+Interpreteur des commandes
+"""
+
+class CmdInterpreterInterface:
+
+	def buildMsg(self, words):
+		"""
+		Construit un message à partir d'une commande
+		Fonction à réimplémenter
+		words: mots qui composent la commande
+		"""
+		pass
---
+++ velov-embedded/net_event_handler.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Géstionnaire des intérruptions réseau
+"""
+
+from event_handler_interface import EventHandlerInterface
+from se_states import SystemState
+
+
+class NetworkEventHandler(EventHandlerInterface):
+
+	def execute(self, args):
+		words = args.split()
+		if len(words) == 0:
+			self._err_msg = "Message réseau reçu vide"
+			self._sendAnswer(False)
+			return False 
+		# On examine les différents cas
+		if words[0] == "CHG":
+			# Demande de changement d'état du SE
+			if len(words) != 5:
+				self._err_msg = "Message réseau de changement d'état invalide"
+				self._sendAnswer(False)
+				return False
+			if words[3] not in SystemState.StrToState:
+				self._err_msg = "Etat cible inconnu"
+				self._sendAnswer(False)
+				return False
+			if not self._se_state.setState(SystemState.StrToState[words[3]]):
+				self._err_msg = "Demande de changement d'état incohérant"
+				self._sendAnswer(False)
+				return False
+		else:
+			# Message inconnue
+			self._err_msg = "Message réseau inconnu"
+			self._sendAnswer(False)
+			return False
+		# Happy ending
+		self._sendAnswer(True)
+		return True
+
+	def _sendAnswer(self, ans):
+		ans_str = None
+		if (ans):
+			ans_str = "OK"
+		else:
+			ans_str = "NOK"
+		ans_str += self._serv_com.getTimestamp() + " " + self._se_state.GetState()
+		self._serv_com.sendData(ans_str)
