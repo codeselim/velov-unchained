26d28a2608cc3ff7172a1725e518660dd64df303
Charlotte <charlotte@debian>
2014-01-23 17:42:01 +0100

    Reservation dans interface

--- web.py/view.py
+++ web.py/view.py
@@ -6,8 +6,7 @@ import config
 t_globals = dict(
   datestr=web.datestr, 
 )
-render = web.template.render('templates/', cache=config.cache, 
-    globals=t_globals)
+render = web.template.render('templates/', cache=config.cache, globals=t_globals)
 render._keywords['globals']['render'] = render
 
 def listing(**k):
